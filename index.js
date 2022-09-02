6a8714db4269a39c23755c861e38a2d7298bc7e8
mbpfx <mbpfx@mail.com>
2022-09-02 11:51:46 +0200

    berekenBoven added

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ De gebruiker typt per land de naam en de oppervlakte. Hij doet dit tot hij stop 
 Toon de gemiddelde oppervlakte van de landen.
 Toon de landen met een oppervlakte onder dit gemiddelde.
 Toon per land de naam en de oppervlakte.
-Toon de landen met een oppervlakte boven dit gemiddelde.
-Toon per land de naam en de oppervlakte.
 */
 
 // Helper function
 function tikLanden() {
   const landen = [];
-  let naam = prompt("land");
+  let naam = prompt("Land: (Tik \"stop\" om te stoppen)");
   while (naam !== "stop") {
-    landen.push({ naam: naam, oppervlakte: prompt("Oppervlakte") });
-    naam = prompt("land");
+    while (naam !== "stop" && !(/[a-zA-Z]/g.test(naam))){
+      naam=prompt(`Foute invoer. Land: `)
+    }
+    const oppervlakte = ()=>{
+      let result=prompt("Oppervlakte: ");
+      while(result!=="stop" && !(/[0-9]+/g.test(result))){
+        result=prompt(`Foute invoer. Oppervlakte: `);
+      }
+      return result;
+    }
+    landen.push({ naam: naam, oppervlakte: oppervlakte()});
+    naam = prompt("Land: (Tik \"stop\" om te stoppen)" );
   }
   return landen;
 }
@@ -22,9 +30,12 @@ const landen = tikLanden();
 if (landen.length > 0) {
   function displayAlleLanden(landen_array) {
     landen_array.map((land) => {
+      const ul = document.createElement("ul");
+      ul.className="landen-ul";
       const li = document.createElement("li");
       li.innerHTML = `Land:  ${land.naam} Oppervlakte:  ${land.oppervlakte}`;
-      document.querySelector("#landen-ul").appendChild(li);
+      ul.appendChild(li);
+      document.querySelector(".landen-article").appendChild(ul);
     });
   }
   function berekenGemiddeldeOppervlakte(landen_array) {
@@ -36,8 +47,9 @@ if (landen.length > 0) {
   }
   function displayGemiddelde(gemiddelde) {
     const gem_h2 = document.createElement("h2");
+    gem_h2.className="gemiddelde-title";
     gem_h2.innerText = `Gemiddelde van alle landen: ${gemiddelde}`;
-    document.querySelector("body").append(gem_h2);
+    document.querySelector(".gemiddelde-article").appendChild(gem_h2);
     return gem_h2;
   }
   function berekenOnderGemiddelde(landen_array, landen_gemiddelde) {
@@ -48,23 +60,60 @@ if (landen.length > 0) {
       });
   }
   function displayOnderGemiddelde(onder_gemiddelde_array, landen_array) {
+    const ul = document.createElement("ul");
+    ul.className="onder-gemiddelde-ul";
+    const h2 = document.createElement("h2");
+    h2.className="onder-gemiddelde-title";
+    h2.innerText = `Onder gemiddelde: `;
+    document.querySelector(".onder-gemiddelde-article").append(h2,ul);
+
     const result = landen_array.filter((item) =>
       onder_gemiddelde_array.includes(item.oppervlakte)
     );
-    const h2 = document.createElement("h2");
-    h2.innerText = `Onder gemiddelde: `;
-    document.querySelector("#onder-gemiddelde-ul").appendChild(h2);
     result.map((land) => {
       const li = document.createElement("li");
       li.innerHTML = `Land: ${land.naam}, Oppervlakte: ${land.oppervlakte}`;
-      document.querySelector("#onder-gemiddelde-ul").appendChild(li);
+      document.querySelector(".onder-gemiddelde-ul").appendChild(li);
     });
     return result;
   }
+  function berekenBovenGemiddelde(landen_array, landen_gemiddelde){
+    return landen_array
+    .map((land) => land.oppervlakte)
+    .filter((oppervlakte) => {
+      return oppervlakte > landen_gemiddelde;
+    });
+  }
+  function displayBovenGemiddelde(boven_gemiddelde_array, landen_array) {
+    const ul = document.createElement("ul");
+    ul.className="boven-gemiddelde-ul";
+    const h2 = document.createElement("h2");
+    h2.className="boven-gemiddelde-title";
+    h2.innerText = `Boven gemiddelde: `;
+    document.querySelector(".boven-gemiddelde-article").append(h2,ul);
+
+    const result = landen_array.filter((item) =>
+      boven_gemiddelde_array.includes(item.oppervlakte)
+    );
+    result.map((land) => {
+      const li = document.createElement("li");
+      li.innerHTML = `Land: ${land.naam}, Oppervlakte: ${land.oppervlakte}`;
+      document.querySelector(".boven-gemiddelde-ul").appendChild(li);
+    });
+    return result;
+  }
+  const gemiddeldeOppervlakte=berekenGemiddeldeOppervlakte(landen);
+  const onderGemiddelde=berekenOnderGemiddelde(landen, gemiddeldeOppervlakte);
+  const bovenGemiddelde=berekenBovenGemiddelde(landen, gemiddeldeOppervlakte);
+
   displayAlleLanden(landen);
-  displayGemiddelde(berekenGemiddeldeOppervlakte(landen));
+  displayGemiddelde(gemiddeldeOppervlakte);
   displayOnderGemiddelde(
-    berekenOnderGemiddelde(landen, berekenGemiddeldeOppervlakte(landen)),
+    onderGemiddelde,
+    landen
+  )
+  displayBovenGemiddelde(
+    bovenGemiddelde,
     landen
   );
 }
